8b40c4dbc4f39dbb3bd8a13b87d2477856a9d94a
ansorokey <ansorokey@gmail.com>
2023-11-10 12:34:53 -0500

    much better canvas scaling

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 // reference the canvas element
 const canvas = document.querySelector('canvas');
-// this will be the canvas context
+// I SPENT AN HOUR AND A HALF TRYING TO FIGURE OUT WHY SCALE DIDNT MATCH
+// DON'T TOUCH THIS - CSS DOESNT WORK THE SAME
+canvas.width = 1024;
+canvas.height = 576;
+
 // this is an object that holds all the canvas drawing methods
 const c = canvas.getContext('2d');
+// draw a white rectangle for the game screen
 c.fillStyle = 'white';
-c.fillRect(0, 0, canvas.width / 2, canvas.height / 2) //, y,
+c.fillRect(0, 0, canvas.width, canvas.height);
 
+// the playr sprite
 const playerImage = new Image();
 playerImage.src = './docs/assets/Images/playerDown.png';
 
 // creates an image object to work with
-const image = new Image();
-image.src = './docs/assets/Tiled/Pellet Town.png';
-// image.src = './docs/assets/sprites/my-map.png';
-
+const map = new Image();
+map.src = './docs/assets/Tiled/Pellet Town.png';
 
 //since the image can take a while to create based on size,
 // we want to wait until its loaded to try drawing it to the canvas
-image.onload = async () => {
-    c.drawImage(image, -809, -570) // we can offset the top left corner to change the starting position of the image
+map.onload = () => {
+    c.drawImage(map, -740, -600) // we can offset the top left corner to change the starting position of the image
     // the player image should load after the background
     c.drawImage(
         playerImage, // the player sprite
@@ -26,9 +30,14 @@ image.onload = async () => {
         0, // crop start y
         playerImage.width/4, // crop end x
         playerImage.height, //crop end y
-        canvas.width / 2 - (playerImage.width / 4),
+        canvas.width / 2 - (playerImage.width / 4) / 2,
         canvas.height / 2 - playerImage.height / 2,
-        playerImage.width/4, // crop end x
+        playerImage.width / 4,
         playerImage.height,
     )
 }
+
+window.addEventListener('keydown', (e) => {
+    // console.log(e.target.key)
+
+});
