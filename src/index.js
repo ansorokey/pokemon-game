dc6716181c7d8c1a1701d21057b7e4486d16b5a2
ansorokey <ansorokey@gmail.com>
2024-09-07 23:44:27 -0400

    comments

--- src/index.js
+++ src/index.js
@@ -8,7 +8,38 @@ canvas.height = 576;
 // The canvas context gives us access to the entire canvas API and everything we can do with a canvas
 const c = canvas.getContext('2d');
 
-// drawing
+// drawing the canvas
 c.fillStyle = 'white';
-// PARAMS: x1, y1, x2, y2
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+const mapImage = new Image();
+mapImage.src = '/assets/images'
+
+// drawing the map onto the canvas
+// to draw an image, we need to pass an html image object
+c.drawImage(
+
+)
+
+// const canvas = document.querySelector('canvas');
+// // Doing this in JS
+// // Could do in CSS, but issues can arize when loading css properties
+// canvas.width = 1024;
+// canvas.height = 576;
+
+
+// // The canvas context gives us access to the entire canvas API and everything we can do with a canvas
+// const c = canvas.getContext('2d');
+
+// // drawing the canvas
+// c.fillStyle = 'white';
+// c.fillRect(0, 0, canvas.width, canvas.height)
+
+// const mapImage = new Image();
+// mapImage.src = '/assets/images'
+
+// // drawing the map onto the canvas
+// // to draw an image, we need to pass an html image object
+// c.drawImage(
+
+// )
